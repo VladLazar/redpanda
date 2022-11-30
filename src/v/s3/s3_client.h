9aff34834d0173109217e6fdcca03b247e757b02
Vlad Lazar <vlad@redpanda.com>
2022-11-30 16:59:49 +0000

    cloud_storage: add cloud storage client interface
    This commit introduces a 'client' abstract class to serve as an
    interface for storage clients targeting different cloud providers or
    APIs.

--- src/v/s3/s3_client.h
+++ src/v/s3/s3_client.h
@@ -11,22 +11,15 @@
 #pragma once
 
 #include "cloud_roles/apply_credentials.h"
-#include "http/client.h"
-#include "model/fundamental.h"
 #include "outcome.h"
+#include "s3/client.h"
 #include "s3/client_probe.h"
-#include "s3/configuration.h"
 
 #include <seastar/core/lowres_clock.hh>
 
 #include <chrono>
 
 namespace s3 {
-
-struct object_tag {
-    ss::sstring key;
-    ss::sstring value;
-};
 
 /// Request formatter for AWS S3
 class request_creator {
@@ -98,7 +91,7 @@ private:
 };
 
 /// S3 REST-API client
-class s3_client {
+class s3_client : public client {
 public:
     s3_client(
       const configuration& conf,
@@ -111,42 +104,43 @@ public:
         apply_credentials);
 
     /// Stop the client
-    ss::future<> stop();
-    /// Shutdown the underlying connection
-    void shutdown();
+    ss::future<> stop() override;
 
-    /// Download object from S3 bucket
+    /// Shutdown the underlying connection
+    void shutdown() override;
+
+    /// Download object from S3 using GetObject
     ///
     /// \param name is a bucket name
     /// \param key is an object key
     /// \param timeout is a timeout of the operation
-    /// \param expect_no_such_key log 404 as warning if set to false
-    /// \return future that gets ready after request was sent
+    /// \param expect_no_such_key log missing key events as warnings if false
+    /// \return future that becomes ready after request was sent
     ss::future<http::client::response_stream_ref> get_object(
       bucket_name const& name,
       object_key const& key,
       const ss::lowres_clock::duration& timeout,
-      bool expect_no_such_key = false);
+      bool expect_no_such_key = false) override;
 
-    struct head_object_result {
-        uint64_t object_size;
-        ss::sstring etag;
-    };
-
-    /// HeadObject request.
+    /// Get metadata for object from S3 using HeadObject
+    ///
     /// \param name is a bucket name
-    /// \param key is an id of the object
+    /// \param key is an object key
+    /// \param timeout is a timeout of the operation
     /// \return future that becomes ready when the request is completed
     ss::future<head_object_result> head_object(
       bucket_name const& name,
       object_key const& key,
-      const ss::lowres_clock::duration& timeout);
+      const ss::lowres_clock::duration& timeout) override;
 
-    /// Put object to S3 bucket.
+    /// Upload object to S3 using PutObject
+    ///
     /// \param name is a bucket name
     /// \param key is an id of the object
     /// \param payload_size is a size of the object in bytes
     /// \param body is an input_stream that can be used to read body
+    /// \param tags is a list of kv tags to attach to the object
+    /// \param timeout is a timeout of the operation
     /// \return future that becomes ready when the upload is completed
     ss::future<> put_object(
       bucket_name const& name,
@@ -154,31 +148,34 @@ public:
       size_t payload_size,
       ss::input_stream<char>&& body,
       const std::vector<object_tag>& tags,
-      const ss::lowres_clock::duration& timeout);
+      const ss::lowres_clock::duration& timeout) override;
 
-    struct list_bucket_item {
-        ss::sstring key;
-        std::chrono::system_clock::time_point last_modified;
-        size_t size_bytes;
-        ss::sstring etag;
-    };
-    struct list_bucket_result {
-        bool is_truncated;
-        ss::sstring prefix;
-        std::vector<list_bucket_item> contents;
-    };
+    /// List the objects in the S3 bucket using ListObjectsV2
+    ///
+    /// \param name is a bucket name
+    /// \param prefix optional prefix of objects to list
+    /// \param start_after optional object key to start listing after
+    /// \param max_keys optional upper bound on the number of returned keys
+    /// \param body is an input_stream that can be used to read body
+    /// \return future that becomes ready when the request is completed
     ss::future<list_bucket_result> list_objects_v2(
       const bucket_name& name,
       std::optional<object_key> prefix = std::nullopt,
       std::optional<object_key> start_after = std::nullopt,
       std::optional<size_t> max_keys = std::nullopt,
       const ss::lowres_clock::duration& timeout
-      = http::default_connect_timeout);
+      = http::default_connect_timeout) override;
 
+    /// Delete object from S3 using DeleteObject
+    ///
+    /// \param name is a bucket name
+    /// \param key is an object key
+    /// \param timeout is a timeout of the operation
+    /// \return future that becomes ready when the request is completed
     ss::future<> delete_object(
       const bucket_name& bucket,
       const object_key& key,
-      const ss::lowres_clock::duration& timeout);
+      const ss::lowres_clock::duration& timeout) override;
 
 private:
     request_creator _requestor;
